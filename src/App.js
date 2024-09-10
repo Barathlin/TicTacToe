69ce3dde7f25903dea76c331b7ec1a30152e2622
BARATHLIN <112534630+Barathlin@users.noreply.github.com>
2024-09-10 22:29:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Tictactoe from './components/tictactoe';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Tictactoe/>
+    
+    </div>
+  );
+}
+
+export default App;
